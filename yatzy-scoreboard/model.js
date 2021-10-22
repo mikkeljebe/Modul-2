ec974bdbd5cdaacfad10959e94607b155940812f
mikkeljebe <86236162+mikkeljebe@users.noreply.github.com>
2021-10-22 14:43:41 +0200

    pojoi

---
+++ yatzy-scoreboard/model.js
@@ -0,0 +1,28 @@
+let model = [
+    
+  {label: '', score: ['Per', 'Pål', 'Espen'],},
+  {label: 'Enere', },
+  {label: 'Toere', },
+  {label: 'Treere', },
+  {label: 'Firere', },
+  {label: 'Femmere', },
+  {label: 'Seksere', },
+  {label: 'Sum', }, 
+  {label: 'Bonus',  },
+  {label: '1 par', },
+  {label: '2 par', },
+  {label: '3 like', },
+  {label: '4 like', },
+  {label: 'Liten straight', },
+  {label: 'Stor straight', },
+  {label: 'Hus',}, 
+  {label: 'Sjanse',},
+  {label: 'Yatzy', },
+  {label: 'Totalsum', }
+  
+];
+
+let scoreBoard = ['Mikkel'];
+let user = ['Per'];
+let players = 3;
+
