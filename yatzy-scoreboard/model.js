56c077931346c4b388da98c2ab9eaa090c21bb19
mikkeljebe <86236162+mikkeljebe@users.noreply.github.com>
2021-10-22 15:25:28 +0200

    kkj

--- yatzy-scoreboard/model.js
+++ yatzy-scoreboard/model.js
@@ -1,28 +1,29 @@
 let model = [
     
-  {label: '', score: ['Per', 'Pål', 'Espen'],},
-  {label: 'Enere', },
-  {label: 'Toere', },
-  {label: 'Treere', },
-  {label: 'Firere', },
-  {label: 'Femmere', },
-  {label: 'Seksere', },
-  {label: 'Sum', }, 
-  {label: 'Bonus',  },
-  {label: '1 par', },
-  {label: '2 par', },
-  {label: '3 like', },
-  {label: '4 like', },
-  {label: 'Liten straight', },
-  {label: 'Stor straight', },
-  {label: 'Hus',}, 
-  {label: 'Sjanse',},
-  {label: 'Yatzy', },
-  {label: 'Totalsum', }
+  {label: {txt: '', underline: 'underline'}, score: ['Per', 'Pål', 'Espen'],},
+  {label: {txt: 'Enere'} },
+  {label: {txt: 'Toere'} },
+  {label: {txt: 'Treere'} },
+  {label: {txt: 'Firere'} },
+  {label: {txt: 'Femmere'} },
+  {label: {txt: 'Seksere', underline: 'underline'} },
+  {label: {txt: 'Sum'} }, 
+  {label: {txt: 'Bonus'}  },
+  {label: {txt: '1 par'} },
+  {label: {txt: '2 par'} },
+  {label: {txt: '3 like'} },
+  {label: {txt: '4 like'} },
+  {label: {txt: 'Liten straight'} },
+  {label: {txt: 'Stor straight'} },
+  {label: {txt: 'Hus'}}, 
+  {label: {txt: 'Sjanse'}},
+  {label: {txt: 'Yatzy', underline: 'underline', bold: 'bold'}},
+  {label: {txt: 'Totalsum'} }
   
 ];
 
 let scoreBoard = ['Mikkel'];
 let user = ['Per'];
 let players = 3;
+
 
